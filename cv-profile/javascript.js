7b13be4de47c4680354b972f9072a41b406d230e
Olivier Girardot <olive_girardot@hotmail.com>
2020-06-02 17:31:21 +0200

    trying different options for banner image or video

---
+++ cv-profile/javascript.js
@@ -0,0 +1,8 @@
+/* document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("mouseover", (event) => {
+    event.currentTarget.classList.toggle("img-circle");
+  });
+}); */
+console.log("Hello World");
+
+const addButton = document.querySelector(".lin-kcard");
